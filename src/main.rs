a370698aefb1bf590e532fdd301d989451bf7257
cndoit18 <cndoit18@outlook.com>
2024-10-26 17:48:05 +0800

    add map and player

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
-use bracket_lib::prelude::*;
+mod map;
+mod map_builder;
+mod player;
 
-struct State {}
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
+}
+
+use prelude::*;
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        Self {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_state),
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket World!")
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50().build()?;
-    main_loop(context, State{})
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+    main_loop(context, State::new())
 }
