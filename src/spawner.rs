6580111dbb8337d36e2f999dffad7e65074ba22e
cndoit18 <cndoit18@outlook.com>
2024-11-02 20:57:47 +0800

    add item

--- src/spawner.rs
+++ src/spawner.rs
@@ -8,6 +8,19 @@ fn orc() -> (i32, String, FontCharType) {
     (2, "orc".to_string(), to_cp437('o'))
 }
 
+pub fn spawn_entity(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point,
+){
+    let roll = rng.roll_dice(1,6);
+    match roll {
+        1 => spawn_healing_potion(ecs, pos),
+        2 => spawn_magic_mapper(ecs, pos),
+        _ => spawn_monster(ecs, rng,pos),
+    }
+}
+
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
     let (hp, name, glyph) = match rng.roll_dice(1, 10) {
         1..8 => goblin(),
@@ -56,5 +69,31 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             max: 10,
         },
         FieldOfView::new(8),
+    ));
+}
+
+pub fn spawn_healing_potion(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Item,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('!'),
+        },
+        Name("Healing Potion".to_string()),
+        ProvidesHealing { amount: 6 },
+    ));
+}
+
+pub fn spawn_magic_mapper(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Item,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('{'),
+        },
+        Name("Dungeon Map".to_string()),
+        ProvidesDungeonMap,
     ));
 }
--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -3,6 +3,9 @@ use crate::prelude::*;
 #[system]
 #[read_component(Player)]
 #[read_component(Health)]
+#[read_component(Item)]
+#[read_component(Carried)]
+#[read_component(Name)]
 pub fn hud(ecs: &SubWorld) {
     let mut health_query = <&Health>::query().filter(component::<Player>());
     let player_health = health_query.iter(ecs).next().unwrap();
@@ -25,5 +28,21 @@ pub fn hud(ecs: &SubWorld) {
         ),
         ColorPair::new(WHITE, RED),
     );
+
+    let mut item_query = <(&Item, &Name, &Carried)>::query();
+    let mut y = 3;
+
+    let player = <(Entity, &Player)>::query()
+        .iter(ecs)
+        .map(|(entity, _player)| *entity)
+        .next()
+        .unwrap();
+    item_query
+        .iter(ecs)
+        .filter(|(_, _, carried)| carried.0 == player)
+        .for_each(|(_, name, _)| {
+            draw_batch.print(Point::new(3, y), format!("{} : {}", y - 2, &name.0));
+            y += 1;
+        });
     draw_batch.submit(10000).expect("Batch error");
 }
